05cead292100690f9f2481a3e8fc3d3e8e04ef5c
zero1416 <armengod96@gmail.com>
2015-12-07 00:17:14 -0600

    Dibujar
    se encontro una funcion de dibujado se esta adapatando con los nodos
    para funcionar

--- New Unity Project/Assets/Algo.cs
+++ New Unity Project/Assets/Algo.cs
@@ -3,18 +3,31 @@ using System.Collections;
 
 public class Algo : MonoBehaviour {
 
+	public LineRenderer lineRender;
+	private int numberOfPoints = 0;
+
 	// Use this for initialization
 	void Start () {
+		Screen.SetResolution (720, 1280, true);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Touch dedito = Input.GetTouch (0);
-
-		//Vector2 pos = dedito.position;
-		this.gameObject.transform.position=dedito.position;
+		if( Input.GetKey( KeyCode.Mouse0 ) ) {
+			numberOfPoints++;
+			lineRender.SetVertexCount( numberOfPoints );
+			Vector3 mousePos = new Vector3(0,0,0);
+			mousePos = Input.mousePosition;
+			mousePos.z = 1.0f;
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+			lineRender.SetPosition(numberOfPoints - 1, worldPos);
+		}
+		else {
+			numberOfPoints = 0;
+			lineRender.SetVertexCount(0);
+		}
 
 
 	
