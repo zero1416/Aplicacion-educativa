a1da175f7261842e425bc529c65c640037979f53
Arturo <armengod96@gmail.com>
2015-12-06 18:09:24 -0600

    cambios a touch
    los cambios al touch estaban bien la carama esta mas pequeña por eso se
    sale por de la pantalla

---
+++ New Unity Project/Assets/Algo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Algo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Touch dedito = Input.GetTouch (0);
+
+		//Vector2 pos = dedito.position;
+		this.gameObject.transform.position=dedito.position;
+
+
+	
+	}
+}
