781a08467efa1945ff5e75307a9f425b2d274010
Arturo <armengod96@gmail.com>
2015-12-18 00:04:41 -0600

    Cambios
    Miles de cambios apunto de la beta

--- New Unity Project/Assets/mover.cs
+++ New Unity Project/Assets/mover.cs
@@ -5,12 +5,17 @@ public class mover : MonoBehaviour {
 
 	public LineRenderer lineRender;
 	private int numberOfPoints = 0;
-	bool toca;
+	public GameObject nodo3;
+	bool nodo1, nodo2, nodoo3, ganar;
+
 
 
 	// Use this for initialization
 	void Start () {
-		toca = false;
+		nodo1 = false;
+		nodo2 = false;
+		nodoo3 = false;
+		ganar = false;
 	}
 	
 	// Update is called once per frame
@@ -22,20 +27,40 @@ public class mover : MonoBehaviour {
 			Vector2 worlpos=Camera.main.ScreenToWorldPoint(dedito.position);
 			transform.position=worlpos;
 		}
-		if (toca == true) {
-			if (Input.GetKey (KeyCode.Mouse0)) {
-				numberOfPoints++;
-				lineRender.SetVertexCount (numberOfPoints);
-				Vector3 mousePos = new Vector3 (0, 0, 0);
-				mousePos = Input.mousePosition;
-				mousePos.z = 1.0f;
-				Vector3 worldPos = Camera.main.ScreenToWorldPoint (mousePos);
-				lineRender.SetPosition (numberOfPoints - 1, worldPos);
-			} 
-			if(Input.touchCount == 0) {
-				numberOfPoints = 0;
-				lineRender.SetVertexCount (0);
+		switch(dedito.phase){
+			case TouchPhase.Began:
+
+			break;
+			case TouchPhase.Moved:
+			if (nodo1 == true) {
+				if (Input.GetKey (KeyCode.Mouse0)) {
+					numberOfPoints++;
+					lineRender.SetVertexCount (numberOfPoints);
+					Vector3 mousePos = new Vector3 (0, 0, 0);
+					mousePos = Input.mousePosition;
+					mousePos.z = 1.0f;
+					Vector3 worldPos = Camera.main.ScreenToWorldPoint (mousePos);
+					lineRender.SetPosition (numberOfPoints - 1, worldPos);
+				}
+				if(nodo1 == true && nodo2 == true)
+				{
+					nodo3.SetActive(true);
+				}
+				
 			}
+			break;
+			case TouchPhase.Ended:
+				if(nodo1== false || nodo2== false)
+				{
+					numberOfPoints = 0;
+					lineRender.SetVertexCount (0);
+				}
+				if( nodo1== true && nodo2== true && nodoo3== true)
+				{
+				ganar= true;
+				this.transform.
+				}
+			break;
 
 		}
 	
@@ -45,8 +70,18 @@ public class mover : MonoBehaviour {
 	{
 		Debug.Log ("tocando 2d");
 		if (col.gameObject.name == "Moneda1") {
-			toca=true;
+			nodo1 = true;
 		}
+		if (col.gameObject.name == "Moneda2") {
+			nodo2 = true;
+		}
+		if (col.gameObject.name == "Moneda3") {
+			nodoo3= true;
+		}
+
+	}
+	void OnTriggerExit2D(Collider2D col)
+	{
 
 	}
 }
