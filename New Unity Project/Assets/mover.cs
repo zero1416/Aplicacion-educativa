ae845a303cd1380bbb62ebcbbd52a96a09d50b64
zero1416 <armengod96@gmail.com>
2016-01-04 22:16:51 -0600

    Version lunes
    Corregidos algunos errores un faltan

--- New Unity Project/Assets/mover.cs
+++ New Unity Project/Assets/mover.cs
@@ -5,8 +5,12 @@ public class mover : MonoBehaviour {
 
 	public LineRenderer lineRender;
 	private int numberOfPoints = 0;
-	public GameObject gnodo1,gnodo2,gnodo3, win1, win2;
-	bool nodo1, nodo2, nodoo3, ganar, guia1, guia2;
+	public GameObject gnodo1,gnodo2,gnodo3,gnodo4, win, Bm, Bs;
+	public GameObject gguia1, gguia2, gguia3, gguia4;
+	Vector3 PosBm, PosBs;
+	bool nodo1, nodo2, nodo3, nodo4, ganar; 
+	bool guia1, guia2, guia3, guia4;
+	int c;
 
 
 
@@ -14,10 +18,16 @@ public class mover : MonoBehaviour {
 	void Start () {
 		nodo1 = false;
 		nodo2 = false;
-		nodoo3 = false;
+		nodo3 = false;
+		nodo4 = false;
 		ganar = false;
 		guia1 = false;
 		guia2 = false;
+		guia3 = false;
+		guia4 = false;
+		PosBm = new Vector3 (-1.0f, -0.8f, 0f);
+		PosBs = new Vector3 (1.0f, -0.8f, 0f);
+		c = 0;
 	}
 	
 	// Update is called once per frame
@@ -29,12 +39,18 @@ public class mover : MonoBehaviour {
 			Vector2 worlpos=Camera.main.ScreenToWorldPoint(dedito.position);
 			transform.position=worlpos;
 		}
-		if (nodo1 == true && nodo2 == true && nodoo3 == true && numberOfPoints!=0) {
+		if (nodo1 == true && nodo2 == true && nodo3 == true && nodo4 == true&& numberOfPoints!=100) {
 
 			ganar= true;
-			win1.SetActive(true);
-			win2.SetActive(true);
-			win1.GetComponent<AudioSource>().Play();
+			if(ganar==true && c==0)
+			{
+			win.SetActive(true);
+				Bm.transform.position=Camera.main.ScreenToWorldPoint(PosBm);
+				Bs.transform.position=Camera.main.ScreenToWorldPoint(PosBs);
+			win.GetComponent<AudioSource>().Play();
+
+			c=1;
+			}
 		}
 
 		switch(dedito.phase){
@@ -55,15 +71,47 @@ public class mover : MonoBehaviour {
 					Vector3 worldPos = Camera.main.ScreenToWorldPoint (mousePos);
 					lineRender.SetPosition (numberOfPoints - 1, worldPos);
 				}
-				if(nodo2== true&& nodo1== true)
+				if(nodo2 == true && nodo1 == true)
 				{
 					gnodo3.SetActive(true);
+					if(nodo3 == true)
+					{
+						gnodo4.SetActive(true);
+					}
+				}
+
+				if ( nodo1 == true)
+				{
+					gguia1.SetActive(true);
+					if( nodo2 == true)
+					{
+						gguia2.SetActive(true);
+						if(nodo3 == true)
+						{
+							gguia3.SetActive(true);
+							if(nodo4 == true)
+							{
+								gguia4.SetActive(true);
+							}
+						}
+					}
+				}
+
+				if(nodo1 == false)
+				{
+					nodo2= false;
+					nodo3= false ;
+					nodo4= false ;
+					guia1=false ;
+					guia2=false;
+					guia3=false;
+					guia4=false;
 				}
 				
 			}
 			break;
 			case TouchPhase.Ended:
-			if(nodo2 == false|| nodoo3== false)
+			if(nodo2 == false|| nodo3 == false || nodo4 == false)
 				{
 					numberOfPoints = 0;
 					lineRender.SetVertexCount (0);
@@ -78,35 +126,48 @@ public class mover : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D col)
 	{
-		Debug.Log ("tocando 2d");
+
 		if (col.gameObject.name == "Moneda1") {
 			nodo1 = true;
-			}
-		if (col.gameObject.name == "Moneda2") {
-			nodo2 = true;
-		} 
-		if (col.gameObject.name == "Moneda3") {
-			nodoo3 = true;
 		}
 		if (col.gameObject.name == "guia1") {
-			guia1 = true;
+				guia1 = true;
+		}
+		if (col.gameObject.name == "Moneda2") {
+				nodo2 = true;
 		}
 		if (col.gameObject.name == "guia2") {
-			guia2 = true;
-		} 
+				guia2 = true;
+		}
+		if (col.gameObject.name == "Moneda3") {
+				nodo3 = true;
+		}
+		if (col.gameObject.name == "guia3") {
+				guia3 = true;
+		}
+		if (col.gameObject.name == "Moneda4") {
+				nodo4 = true;
+		}
+		if (col.gameObject.name == "guia4") {
+				guia4 = true;
+		}
+		if (col.gameObject.name == "Menu") {
+			Application.LoadLevel("Seleccionar");
+		}
+
 
 	}
 	void OnTriggerExit2D(Collider2D col)
 	{
-		if (nodo1 == false) {
-		if(col.gameObject.name == "Moneda2"){
-				nodo2=false ;
-			}
-			if(col.gameObject.name=="guia1")
-			{
-				guia1= false;
-			}
-		}
+		/*if(col.gameObject.name != "Moneda1") {
+			nodo2= false;
+			nodo3= false ;
+			nodo4= false ;
+			guia1=false ;
+			guia2=false;
+			guia3=false;
+			guia4=false;
+		}*/
 		if (col.gameObject.name == "guia1") {
 			if(guia2!=true)
 			{
@@ -114,6 +175,22 @@ public class mover : MonoBehaviour {
 			lineRender.SetVertexCount (0);
 			}
 		}
+		if (col.gameObject.name == "guia2") {
+			if(guia3!=true)
+			{
+				numberOfPoints = 0;
+				lineRender.SetVertexCount (0);
+			}
+		}
+		if (col.gameObject.name == "guia3") {
+			if(guia4!=true)
+			{
+				numberOfPoints = 0;
+				lineRender.SetVertexCount (0);
+			}
+		}
+
+
 
 	}
 }
